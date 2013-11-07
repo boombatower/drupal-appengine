9cffb0258028c585d7fcd93c84556b94ed8a060f
boombatower <boombatower@google.com>
2013-11-07 13:47:41 -0800

    Update app.yaml and wrapper.php from google_appenegine module.

--- wrapper.php
+++ wrapper.php
@@ -16,7 +16,7 @@
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 // Provide mod_rewrite like functionality. If a php file in the root directory
-// is explicitely requested then load the file, otherwise load index.php and
+// is explicitly requested then load the file, otherwise load index.php and
 // set get variable 'q' to $_SERVER['REQUEST_URI'].
 if (dirname($path) == '/' && pathinfo($path, PATHINFO_EXTENSION) == 'php') {
   $file = pathinfo($path, PATHINFO_BASENAME);
